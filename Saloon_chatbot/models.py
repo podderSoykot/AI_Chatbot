210d9534d6b1c0d68bff8ca34f6450743c19c33c
podderSoykot <diptopodder95@gmail.com>
2025-08-04 18:21:58 +0600

    structure added for saloon chatbot

---
+++ Saloon_chatbot/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column,Integer,String,DateTime
+from database import Base 
+
+
+#SQLAlchemy Model for Bookings
+class Booking(Base):
+    id=Column(Integer,primary_key=True,index=True)
+    client_name=Column(String,nullable=False)
+    service=Column(String)
+    start_time=Column(DateTime,nullable=False)
+    end_time=Column(DateTime,nullable=False)
